2444bad2d4284418221813401c94761f81c9cc86
reedphish <reedphish@protonmail.com>
2017-03-31 19:58:59 +0100

    Added some excpetion handling when encoding images

--- encode-image.py
+++ encode-image.py
@@ -8,7 +8,10 @@ new_image_path = sys.argv[3]
 
 print("Image encoder - Reedphish Heavy Industries 2017")
 
-encoded_text = TextTransform.encode(open(clear_text_file, "r").read())
-encoding_key = ImageHandler.encode(base_image_path, new_image_path, encoded_text)
+try:
+	encoded_text = TextTransform.encode(open(clear_text_file, "r").read())
+	encoding_key = ImageHandler.encode(base_image_path, new_image_path, encoded_text)
 
-print("\nImage {0} encoded with key {1}\n".format(new_image_path, encoding_key))
+	print("\nImage {0} encoded with key {1}\n".format(new_image_path, encoding_key))
+except Exception as e:
+	print("[ERROR] Unable to encode image: {0}".format(e))
