3c99adc08629dc3edbaa346ba912056fccc34592
reedphish <reedphish@protonmail.com>
2017-03-31 20:24:30 +0100

    Added argparse to driver scripts

--- decode-image.py
+++ decode-image.py
@@ -1,16 +1,19 @@
 from lib.texttransform import TextTransform
 from lib.imagehandler import ImageHandler
-import sys
-
-clear_text_file = sys.argv[1]
-image_path = sys.argv[2]
-key = sys.argv[3]
+import sys, argparse
 
 print("Image decoder - Reedphish Heavy Industries 2017")
 
-decoded_text = ImageHandler.decode(image_path, int(key))
-file = open(clear_text_file, "w")
+parser = argparse.ArgumentParser(description="Encode image with secret text")
+parser.add_argument("outputfilepath", metavar="ofp", type=str, help="Path where to write extracted content")
+parser.add_argument("encodedimagepath", metavar="eip", type=str, help="Path image to decode")
+parser.add_argument("key", metavar="key", type=int, help="Key to decode")
+
+args = parser.parse_args()
+
+decoded_text = ImageHandler.decode(args.encodedimagepath, args.key)
+file = open(args.outputfilepath, "w")
 file.write(decoded_text)
 file.close()
 
-print("\nImage {0} decoded using key {1}. Written to {2}\n".format(image_path, key, clear_text_file))
+print("\nImage {0} decoded using key {1}. Written to {2}\n".format(args.encodedimagepath, args.key, args.outputfilepath))
